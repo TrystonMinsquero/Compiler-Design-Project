e2ce943e5c07270b7ad0af2fc8c3e65a6dac09cb
Tryston Minsquero <tam190005@utdallas.edu>
2022-10-29 12:26:06 -0500

    Update LiteralExpression.java

--- LiteralExpression.java
+++ LiteralExpression.java
@@ -2,6 +2,11 @@ public class LiteralExpression extends Expression{
     private String value;
     private String type;
 
+    public LiteralExpression(boolean b) {
+        this.value = String.valueOf(b);
+        this.type = "bool";
+    }
+
     public LiteralExpression(String s, String type) {
         this.value = s;
         this.type = type;
