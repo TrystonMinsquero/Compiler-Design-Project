05b50d11537ed4e8a960c465e5a2576071ff4f01
Tryston Minsquero <tam190005@utdallas.edu>
2022-10-29 15:18:34 -0500

    hmph

--- AssignStatement.java
+++ AssignStatement.java
@@ -1,17 +1,17 @@
 public class AssignStatement extends Statement {
-    private String name;
+    private Name name;
     private Expression expr;
 
     public AssignStatement(Name n, Expression e) {
-        name = n.toString(0);
-        expr = e;
-    }
-
-    public AssignStatement(String n, Expression e) {
         name = n;
         expr = e;
     }
 
+    public AssignStatement(String n, Expression e) {
+        name = new Name(n);
+        expr = e;
+    }
+
     public String toString(int t) {
-        return getTabs(t) + name + " = " + expr.toString(0) + ";\n"; }
+        return getTabs(t) + name.toString(0) + " = " + expr.toString(0) + ";\n"; }
 }
