2d633639ccb2a627833dfa1e2313f40cd4439d04
Tryston Minsquero <tam190005@utdallas.edu>
2022-10-29 12:25:24 -0500

    stringify

--- LiteralExpression.java
+++ LiteralExpression.java
@@ -2,26 +2,6 @@ public class LiteralExpression extends Expression{
     private String value;
     private String type;
 
-    public LiteralExpression(Integer i) {
-        this.value = String.valueOf(i);
-        this.type = "int";
-    }
-
-    public LiteralExpression(Float f) {
-        this.value = String.valueOf(f);
-        this.type = "float";
-    }
-
-    public LiteralExpression(boolean b) {
-        this.value = String.valueOf(b);
-        this.type = "bool";
-    }
-
-    public LiteralExpression(Character c) {
-        this.value = String.valueOf(c);
-        this.type = "char";
-    }
-
     public LiteralExpression(String s, String type) {
         this.value = s;
         this.type = type;
