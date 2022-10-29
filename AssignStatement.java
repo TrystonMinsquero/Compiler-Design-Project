1c5fe5098522cfdc1392d595f48bc13b6a87a66b
Tryston Minsquero <tam190005@utdallas.edu>
2022-10-29 11:46:32 -0500

    Update AssignStatement.java

--- AssignStatement.java
+++ AssignStatement.java
@@ -1,12 +1,17 @@
 public class AssignStatement extends Statement {
-    private Name name;
+    private String name;
     private Expression expr;
 
     public AssignStatement(Name n, Expression e) {
+        name = n.toString(0);
+        expr = e;
+    }
+
+    public AssignStatement(String n, Expression e) {
         name = n;
         expr = e;
     }
 
     public String toString(int t) {
-        return getTabs(t) + name.toString(0) + " = " + expr.toString(0) + ";\n"; }
+        return getTabs(t) + name + " = " + expr.toString(0) + ";\n"; }
 }
