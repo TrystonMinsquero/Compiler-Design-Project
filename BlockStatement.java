b97e19fe438ff84affa3bb6e194450a17134b451
Tryston Minsquero <tam190005@utdallas.edu>
2022-10-31 15:49:54 -0500

    block

--- BlockStatement.java
+++ BlockStatement.java
@@ -2,6 +2,11 @@ public class BlockStatement extends Statement{
     private Statements statements;
     private String semiColon;
 
+    public BlockStatement(String optionalSemiColon) {
+        statements = new Statements();
+        semiColon = optionalSemiColon;
+    }
+
     public BlockStatement(Statements s, String optionalSemiColon) {
         statements = s;
         semiColon = optionalSemiColon;
