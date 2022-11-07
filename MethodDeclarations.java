f3c893dff021ea140c26b3d478ccec587e44551c
Tryston Minsquero <tam190005@utdallas.edu>
2022-11-06 18:36:47 -0600

    yuh

--- MethodDeclarations.java
+++ MethodDeclarations.java
@@ -1,6 +1,9 @@
 public class MethodDeclarations extends Declarations{
 
     public String toString(int t) {
+        if(declerations.size() == 0) {
+            return "";
+        }
         String ret = getTabs(t) + "Methods:\n";
         for(Declaration d : declerations)
             ret += d.toString(t+1);
