6691c80f1080b715c73e658ab9c2eeaed399a8ba
Tryston Minsquero <tam190005@utdallas.edu>
2022-11-07 10:31:21 -0600

    arggg

--- ArgDeclarations.java
+++ ArgDeclarations.java
@@ -12,7 +12,8 @@ public class ArgDeclarations extends Declarations{
         String ret = "";
         for(Declaration d : declerations)
             ret += d.toString(0) + ", ";
-
-        return ret.substring(0, ret.length()-2);
+        if(ret.length() > 0)
+            return ret.substring(0, ret.length()-2);
+        return ret;
     }
 }
