0a3d30caaefdd378b8b12569bc7516da4f2a9bd5
Tryston Minsquero <tam190005@utdallas.edu>
2022-11-07 16:51:52 -0600

    full test

--- MemberDeclarations.java
+++ MemberDeclarations.java
@@ -43,7 +43,7 @@ public class MemberDeclarations extends Declarations {
     // }
 
     public String toString(int t) {
-        String ret = "Stupid";
+        String ret = "";
         boolean printedFields = false;
         boolean printedMethods = false;
         for(Declaration d : declerations) {
